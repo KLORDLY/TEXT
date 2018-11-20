4c24bbc6cb30ab87f4ac8a04ba8d1f003e99302e
曾曾 <269571627@qq.com>
2018-11-20 21:52:50 +0800

    fbnq

---
+++ python/python十进制的转换.py
@@ -0,0 +1,13 @@
+Python 3.6.7 (v3.6.7:6ec5cf24b7, Oct 20 2018, 13:35:33) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> dec = int(input("输入数字："))
+输入数字：45
+>>> print("十进制",dec)
+十进制 45
+>>> print("二进制",bin(dec))
+二进制 0b101101
+>>> print("八进制",oct(dec))
+八进制 0o55
+>>> print("十六进制",hex(dec))
+十六进制 0x2d
+>>> 
