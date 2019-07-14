45f290a2ad0f84d8135431c8d9e6214110978ed0
曾曾 <269571627@qq.com>
2019-07-14 17:59:57 +0800

    西西团队

---
+++ wchat/西西团队/miniprogram/pages/openapi/callback/callback.js
@@ -0,0 +1,16 @@
+// miniprogram/pages/openapi/callback/callback.js
+Page({
+
+  data: {
+
+  },
+
+  onLoad: function (options) {
+
+  },
+
+  onCustomerServiceButtonClick(e) {
+    console.log(e)
+    
+  },
+})
